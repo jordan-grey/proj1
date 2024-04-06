c0c0f081f813240dd7d82bafa395c9c74bee32df
jordan-grey <j2reedy17@gmail.com>
2024-04-06 15:52:09 -0700

    added UI and double jump
    this sure is a double jump if i have ever seen one! i can't tell if it's finnicky because i'm running this on a potato or if my code is wonk, both are viable and equally likely! enjoy rollaball, self defenestration is a feature not a bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,20 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    private int count;
+    public GameObject winTextObject;
+    private bool doubleJump;
+    public float jumpForce = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+        doubleJump = false;
     }
 
     void OnMove(InputValue movementValue)
@@ -27,6 +37,22 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+        bool isGrounded = (rb.position.y == 0.5);
+
+        if ((isGrounded))
+        {
+            doubleJump = false;
+            rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded || doubleJump)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+                doubleJump = !doubleJump;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -34,6 +60,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
